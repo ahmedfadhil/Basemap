e8b54ce6dda37119de0e015f9c00293d0304319d
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2017-11-04 16:34:14 +0100

    plottin maps

--- bm.py
+++ bm.py
@@ -1,9 +1,21 @@
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
+import numpy as np
 
-# setup Lambert Conformal basemap.
-# set resolution=None to skip processing of boundary datasets.
-m = Basemap(width=12000000, height=9000000, projection='lcc',
-            resolution=None, lat_1=45., lat_2=55, lat_0=50, lon_0=-107.)
+m = Basemap(projection='mill',
+            llcrnrlat=-90,
+            llcrnrlon=-180,
+            urcrnrlat=90,
+            urcrnrlon=180,
+            resolution='c')
+
+m.drawcoastlines()
+m.drawcountries(linewidth=2)
+##m.drawstates(color='b')
+##m.drawcounties(color='darkred')
+# m.fillcontinents()
+# m.etopo()
 m.bluemarble()
+
+plt.title('Basemap Tutorial')
 plt.show()
